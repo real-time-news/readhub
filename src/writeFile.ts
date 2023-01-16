fa817a1c85d13919e7a8b9821442fb9309e6a14a
孟世博 <897205285@qq.com>
2023-01-16 19:19:50 +0800

    update

--- src/writeFile.ts
+++ src/writeFile.ts
@@ -1,11 +1,28 @@
 import fs from 'fs';
 import path from 'path';
 
-export default function writeFile(data: any) {
+export default async function writeFile(newFileData: any) {
+
     const filePath = path.resolve(__dirname, '../data/2023-01-16.json');
-    fs.writeFile(filePath, JSON.stringify(data), (err: any) => {
-        if (err) {
-            
-        }
+
+    fs.readFile(filePath, (err: any, fileData: any) => {
+
+        const fileDataJson = JSON.parse(fileData.toString());
+        const news = [...fileDataJson]
+
+        newFileData.map(item => {
+            const isExist = fileDataJson.find((fileItem: any) => fileItem.id === item.id)
+            if (!isExist) {
+                fileDataJson.unshift(item)
+            }
+        })
+
+        fs.writeFile(filePath, JSON.stringify(news), (err: any) => {
+            if (err) {
+                console.log(err)
+            }
+        })
     })
+
+
 }
