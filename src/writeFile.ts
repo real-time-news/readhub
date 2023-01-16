c85725bff9d02e4bc1d89c5f32ac86f313dae1f6
孟世博 <897205285@qq.com>
2023-01-16 18:14:52 +0800

    Remove the console expressions (#3)
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- src/writeFile.ts
+++ src/writeFile.ts
@@ -5,7 +5,7 @@ export default function writeFile(data: any) {
     const filePath = path.resolve(__dirname, '../data/2023-01-16.json');
     fs.writeFile(filePath, JSON.stringify(data), (err: any) => {
         if (err) {
-            console.log('err', err);
+            
         }
     })
 }
