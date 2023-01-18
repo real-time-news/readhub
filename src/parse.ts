b78cac2451f0e626087811a5e1d17482b3672981
孟世博 <897205285@qq.com>
2023-01-18 14:15:13 +0800

    Update parse.ts

--- src/parse.ts
+++ src/parse.ts
@@ -11,5 +11,8 @@ export default async function parse(data: any) {
     // Sort the data by date
     data.sort((a: any, b: any) => { return new Date(b.date).getTime() - new Date(a.date).getTime() })
 
+    // filter the data by date
+    data = data.filter((item: any) => { return dayjs(item.date).format("YYYY-MM-DD") === dayjs().format("YYYY-MM-DD") })
+
     return data
 }
